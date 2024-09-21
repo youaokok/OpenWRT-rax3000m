3dadc68dec3a0af4c1ed2c5022011e80b12fbb64
youaokok <zzhl10010@gmail.com>
2024-09-21 21:22:52 +0800

    Update diy-part1.sh

--- hanwckf/CMCC-RAX3000M-EMMC/diy-part1.sh
+++ hanwckf/CMCC-RAX3000M-EMMC/diy-part1.sh
@@ -12,6 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i '/src-git modemfeed/s/^/#/' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
